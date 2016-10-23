48222183aa5528a30d5d1b601b1efdcb0a8628e9
TNathen <truong.nathen1213@gmail.com>
2016-10-23 03:45:16 -0400

    update compare

--- Compare.py
+++ Compare.py
@@ -1,8 +1,10 @@
 import cv2
 import numpy as np
 
-def getCompare(cnt2):
+def getCompare(cnt2, num):
     img = cv2.imread('file.png', 0)
+    if(num == 0):
+    	img = cv2.imread('file2.png',0)
     #img2 = cv2.imread('file.png', 0)
     ret,thresh = cv2.threshold(img,127,255,0)
     cnt1,hierarchy = cv2.findContours(thresh, 1, 2)
@@ -12,6 +14,3 @@ def getCompare(cnt2):
 
     ret = cv2.matchShapes(cnt1[0], cnt2,1, 0.0)
     return ret
-
-
-
