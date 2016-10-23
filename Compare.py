77eb7cb78d5edab3ad2a4d633af46f46f2e2722b
Sid <sidkidop@gmail.com>
2016-10-22 22:43:20 -0400

    update compare

--- Compare.py
+++ Compare.py
@@ -2,14 +2,16 @@ import cv2
 import numpy as np
 
 def getCompare(cnt2):
-    img1 = cv2.imread('file.png', 0)    
+    img = cv2.imread('file.png', 0)
+    #img2 = cv2.imread('file.png', 0)
+    ret,thresh = cv2.threshold(img,127,255,0)
+    cnt1,hierarchy = cv2.findContours(thresh, 1, 2)
 
-    ret, thresh = cv2.threshold(img1, 127, 255,0)
-    #ret, thresh2 = cv2.threshold(img2, 127, 255,0)
-    contours,hierarchy = cv2.findContours(thresh,2,1)
-    cnt1 = contours[0]
-    #contours,hierarchy = cv2.findContours(thresh2,2,1)
-    #cnt2 = contours[0]
+    #ret,thresh = cv2.threshold(img2,127,255,0)
+    #cnt2,hierarchy = cv2.findContours(thresh, 1, 2)
 
-    ret = cv2.matchShapes(cnt1, cnt2,1, 0.0)
+    ret = cv2.matchShapes(cnt1[0], cnt2,1, 0.0)
     return ret
+
+
+
